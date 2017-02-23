361cd59c38778f4b8ef29ad6e7016d4a6c579718
Ipaye Alameen <Akinkunmi007@gmail.com>
2017-02-23 10:45:55 +0100

    implemented the getradius and get volume function

--- Spheres/className.cpp
+++ Spheres/className.cpp
@@ -1,18 +1,26 @@
 #include <iostream>
 #include "Spheres.h"
+#include <cmath>
+const float pi = 3.142;
 using namespace std;
 
-Sphere::Sphere(){
-}
-Sphere::~Sphere(){
+Sphere::Sphere(float iradius, float ivolume){
+    iradius = radius;
+    ivolume = volume;
 }
 
 void Sphere::getRadiusValues(){
+    cout<<"Enter the value of radius: ";
+    cin>>radius;
 }
 
 void Sphere::volumeOfSphere(){
-    
+    volume = (4/3)*(pi)*pow(radius,3);
+    cout<<"The volume of the sphere is :\t"<< volume <<endl;
 }
 
 void Sphere::surfaceArea(){
+}
+
+Sphere::~Sphere(){
 }
