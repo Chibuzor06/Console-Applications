7eca876bbb431399e6c216550593b2e069f6ae54
Ipaye Alameen <Akinkunmi007@gmail.com>
2017-03-12 19:16:02 +0100

    finished up the header declearation

--- testSoln/date.h
+++ testSoln/date.h
@@ -1,11 +1,17 @@
 #ifndef DATE_H
 #define DATE_H
 
-class Date(){
+class Date{
     private:
+    int day, month, year;
 
     public:
-    
-}
+    Date();
+    Date(int m,int d);
+    void getDate();
+    void writeDate();
+    bool valDate();
+    void howManyDays();
+};
 
 #endif
