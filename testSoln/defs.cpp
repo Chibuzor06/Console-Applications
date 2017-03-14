dd79cacb3ed07321b48d02bff42549a7f986892c
Ipaye Alameen <Akinkunmi007@gmail.com>
2017-03-14 04:29:23 +0100

    defined all the methods except for the comparism operator

--- testSoln/defs.cpp
+++ testSoln/defs.cpp
@@ -5,45 +5,60 @@
 using namespace std;
 
 Date::Date(){
-	month = 1;
-	day = 1;
+	month = 0;
+	day = 0;
 }
 Date::Date(int m, int d){
-	m = month = 1;
-	d = day = 1;
+	month = m;
+	day = d;
 	// imonth = month;
 }
 
 
 void Date::getDate(){
+	int m,d;
 	cout<<"Enter a date in this format (month/Day) in figures \n"<<endl;
 	cout<<"Enter the month : ";
-	cin>>month;
+	cin>>m;
 	cout<<"Enter the day : ";
-	cin>>day;
+	cin>>d;
+	day = d;
+	month = m;
 }
 
 void Date::writeDate(){
 	string mnth[] = {"january", "febuary", "march", "april", "may", "june", "july", "august", "september", "october", "november", "december"};
-	cout << mnth[month-1] << " "<<day <<endl;;
+	cout << mnth[month-1] << " "<<day <<endl;
+	cout <<"the no of days so far is : ";
+	howManyDays();
+	cout <<endl;
 }
 
-bool valDate(){
+void Date::setDate(int m,int d){
+	m = month;
+	d = day;
+}
+
+bool Date::valDate(){
+	// int mm = month;
+	// int dd = day;
 	// implement the logic here using an if checker for the months
 	// if ()
 	int noDay[12] = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
-	if (month < 1 || month > 12){
+
+	if((month >= 1) && (month <= 12)){
 		if (day <= noDay[month-1]){
 			return true;
-		};
+		}
 	}
-	
 }
+
 void Date::howManyDays(){
+
 	int noDay[12] = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
 	int sum = 0;
-	if(i=0; i < month; i++){
-		sum =+ noDay[i];
+	for(int i=0; i < (month-1); i++){
+		sum += noDay[i];
 	}
-	cout<< sum + day;
+	cout<< sum+day;
 }
