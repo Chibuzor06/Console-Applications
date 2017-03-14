c7a442f6d191deeaf9975aa3b0073612331bf63d
Ipaye Alameen <Akinkunmi007@gmail.com>
2017-03-14 04:28:33 +0100

    succesfully run code so far without errors

--- testSoln/driver.cpp
+++ testSoln/driver.cpp
@@ -6,8 +6,21 @@ using namespace std;
 int main()
 {
 	Date q;
+	bool valid;
+	char ans;
+	do{
+
 	q.getDate();
 	//q.valDate();
+	valid = q.valDate();
+	if(valid == false){
+		cout<<"\n\nDate is invalid !!! It is reset to jan 01\n\n\n";
+	}
 	q.writeDate();
+	
+	//q.howManyDays();
+	cout  << "Do you wish to continue :";
+	cin >> ans;
+	}while(ans == 'y');
 	return 0;
 }
